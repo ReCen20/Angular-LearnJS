70c4af93b21157641ca770cd85214442705642c4
igimaletdinov <igimaletdinov@diasoft.ru>
2023-10-08 12:53:45 +0400

    E-0003 - practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { applicationConfigMock } from './shared/application-config/application-config.mock';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css'],
+	encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class AppComponent {
 	title = 'Angular-LearnJS-1';
 	readonly log = console.log;
 
+	isSidenavOpened = true;
+
 	readonly applicationConfig = applicationConfigMock;
+
+	toggleSidenavOpened() {
+		this.isSidenavOpened = !this.isSidenavOpened;
+	}
 
 	onClick($event: MouseEvent) {
 		console.log('header click', $event);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderModule } from './core/header/header.module';
+import { SidenavModule } from './core/sidenav/sidenav.module';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule, SidenavModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
